eef9795270ba8645c287f3d6952b11931bac1570
Quan9 <boboylt000@gmail.com>
2024-05-27 17:28:34 +0700

    edit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,6 @@ import "@mantine/core/styles.layer.css";
 import "@mantine/charts/styles.css";
 import "@mantine/carousel/styles.css";
 import "@mantine/dates/styles.css";
-import "@mantine/tiptap/styles.css";
 import "mantine-datatable/styles.layer.css";
 import { useSelector } from "react-redux";
 import { io } from "socket.io-client";
