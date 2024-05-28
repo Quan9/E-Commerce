0976431ccd0d086b94456afc03921944075a35ea
Quan9 <boboylt000@gmail.com>
2024-05-28 10:39:01 +0700

    remove /

--- frontend/src/services/commonMethods.jsx
+++ frontend/src/services/commonMethods.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BASE_URL = "/api";
 const TOKEN = JSON.parse(localStorage.getItem("user"))?.accessToken || null;
-const url = "https://e-commerce-backend-studentquan9-9b1574ae.koyeb.app/";
+const url = "https://e-commerce-backend-studentquan9-9b1574ae.koyeb.app";
 export const publicRequest = axios.create({
   baseURL: url + BASE_URL,
 });
