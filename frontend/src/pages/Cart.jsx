a83c716625d4538ea9db6171a4407d0f7dbe34d2
Quan9 <boboylt000@gmail.com>
2024-05-28 12:56:41 +0700

    update stripe

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -96,8 +96,7 @@ const Cart = () => {
     const stripe = () => {
       if (checkOut) {
         createPaymentIntent({ cart, user }).then((res) => {
-          console.log(res.data.url);
-          window.location.href = res.data.url;
+          nav(res.data.url);
         });
       } else {
         <Notification
