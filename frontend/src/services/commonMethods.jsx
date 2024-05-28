d33afafebf5d5c4f0339fcda3e03c476319d5202
Quan9 <boboylt000@gmail.com>
2024-05-28 10:35:53 +0700

    update

--- frontend/src/services/commonMethods.jsx
+++ frontend/src/services/commonMethods.jsx
@@ -2,13 +2,13 @@ import axios from "axios";
 
 const BASE_URL = "/api";
 const TOKEN = JSON.parse(localStorage.getItem("user"))?.accessToken || null;
-
+const url = "https://e-commerce-backend-studentquan9-9b1574ae.koyeb.app/";
 export const publicRequest = axios.create({
-  baseURL: BASE_URL,
+  baseURL: url + BASE_URL,
 });
 
 export const userRequest = axios.create({
-  baseURL: BASE_URL,
+  baseURL: url + BASE_URL,
   headers: {
     token: TOKEN,
     id: JSON.parse(localStorage.getItem("user"))?._id,
