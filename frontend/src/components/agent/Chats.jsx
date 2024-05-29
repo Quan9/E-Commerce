de288e8b0300ba1ceea4de63a7f3f4ab6f0edde0
Quan9 <boboylt000@gmail.com>
2024-05-29 13:49:44 +0700

    update

--- frontend/src/components/agent/Chats.jsx
+++ frontend/src/components/agent/Chats.jsx
@@ -21,57 +21,61 @@ const Chats = (props) => {
   };
   return (
     <Paper h={"90vh"} style={{ overflowY: "scroll" }}>
-      <Title order={3} ta={"center"}>
-        Chats
-      </Title>
-      <Stack>
-        {Object.entries(chats)
-          ?.sort((a, b) => new Date(b.updatedAt) - new Date(a.updatedAt))
-          .map((chat) => (
-            <Indicator
-              size={17}
-              disabled={checkUnread(chat.latestMessage)}
-              label="New Message"
-              key={chat._id}
-              position="top-left"
-            >
-              <Card
-                onClick={() => {
-                  setSelectedChat(chat);
-                  //   await chatRead(chat);
-                  //   params(chat._id);
-                }}
-                bg={selectedChat === chat ? "#38B2AC" : "#E8E8E8"}
-                color={selectedChat === chat ? "white" : "black"}
-                onMouseEnter={(e) => (e.target.style.cursor = "pointer")}
-              >
-                <CardSection>
-                  <Text size="xl" ta={"center"}>
-                    {chat.chatName.startsWith(`user-`)
-                      ? `AnoUser-${chat.chatName.substr(-5)}`
-                      : `${chat.chatName}`}
-                  </Text>
-                  <Text
-                    span
-                    size="md"
-                    c={checkUnread(chat.latestMessage) ? "gray" : "red"}
+      {chats && (
+        <>
+          <Title order={3} ta={"center"}>
+            Chats
+          </Title>
+          <Stack>
+            {Object.entries(chats)
+              ?.sort((a, b) => new Date(b.updatedAt) - new Date(a.updatedAt))
+              .map((chat) => (
+                <Indicator
+                  size={17}
+                  disabled={checkUnread(chat.latestMessage)}
+                  label="New Message"
+                  key={chat._id}
+                  position="top-left"
+                >
+                  <Card
+                    onClick={() => {
+                      setSelectedChat(chat);
+                      //   await chatRead(chat);
+                      //   params(chat._id);
+                    }}
+                    bg={selectedChat === chat ? "#38B2AC" : "#E8E8E8"}
+                    color={selectedChat === chat ? "white" : "black"}
+                    onMouseEnter={(e) => (e.target.style.cursor = "pointer")}
                   >
-                    {chat?.latestMessage?.sender?.username === chat.chatName &&
-                    chat.isUser === false ? (
-                      <b>{`AnoUser-${chat.chatName.substr(-5)}`}</b>
-                    ) : (
-                      <b>{chat?.latestMessage?.sender?.username}</b>
-                    )}
-                    <b> : </b>
-                    {chat.latestMessage.content.length > 50
-                      ? chat.latestMessage.content.substring(0, 35) + "..."
-                      : chat.latestMessage.content}
-                  </Text>
-                </CardSection>
-              </Card>
-            </Indicator>
-          ))}
-      </Stack>
+                    <CardSection>
+                      <Text size="xl" ta={"center"}>
+                        {chat.chatName.startsWith(`user-`)
+                          ? `AnoUser-${chat.chatName.substr(-5)}`
+                          : `${chat.chatName}`}
+                      </Text>
+                      <Text
+                        span
+                        size="md"
+                        c={checkUnread(chat.latestMessage) ? "gray" : "red"}
+                      >
+                        {chat?.latestMessage?.sender?.username ===
+                          chat.chatName && chat.isUser === false ? (
+                          <b>{`AnoUser-${chat.chatName.substr(-5)}`}</b>
+                        ) : (
+                          <b>{chat?.latestMessage?.sender?.username}</b>
+                        )}
+                        <b> : </b>
+                        {chat.latestMessage.content.length > 50
+                          ? chat.latestMessage.content.substring(0, 35) + "..."
+                          : chat.latestMessage.content}
+                      </Text>
+                    </CardSection>
+                  </Card>
+                </Indicator>
+              ))}
+          </Stack>
+        </>
+      )}
     </Paper>
   );
 };
