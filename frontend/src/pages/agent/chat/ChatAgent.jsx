0873393faeca99285df7694e47de70b2f2a6e7c5
Quan9 <boboylt000@gmail.com>
2024-05-29 15:05:31 +0700

    a

--- frontend/src/pages/agent/chat/ChatAgent.jsx
+++ frontend/src/pages/agent/chat/ChatAgent.jsx
@@ -166,6 +166,12 @@ const ChatAgent = ({ socket }) => {
       setMessages([...messages, data]);
     }
   };
+  useEffect(() => {
+    const a = Object.entries(chats).sort(
+      (a, b) => new Date(b.updatedAt) - new Date(a.updatedAt)
+    );
+    console.log(a, "------");
+  }, [chats]);
   return (
     <Grid>
       {chats ? (
@@ -176,48 +182,47 @@ const ChatAgent = ({ socket }) => {
               {Object.entries(chats)
                 .sort((a, b) => new Date(b.updatedAt) - new Date(a.updatedAt))
                 .map((chat) => (
-                  // <Indicator
-                  //   size={17}
-                  //   disabled={checkUnread(chat.latestMessage)}
-                  //   label="New"
-                  //   key={chat._id}
-                  //   position="top-left"
-                  // >
-                  //   <Paper
-                  //     onClick={() => {
-                  //       setSelectedChat(chat);
-                  //       chatRead(chat);
-                  //       params(chat._id);
-                  //     }}
-                  //     bg={selectedChat === chat ? "#38B2AC" : "#E8E8E8"}
-                  //     color={selectedChat === chat ? "white" : "black"}
-                  //     onMouseEnter={(e) => (e.target.style.cursor = "pointer")}
-                  //   >
-                  //     <Text size="xl" ta={"center"}>
-                  //       {/* {chat.chatName.startsWith("user-") */}
-                  //       {/* ? `AnoUser-${chat.chatName.substr(-5)}` */}
-                  //       {/* : `${chat.chatName}`} */}
-                  //       {chat.chatName}
-                  //     </Text>
-                  //     <Text
-                  //       span
-                  //       size="md"
-                  //       c={checkUnread(chat.latestMessage) ? "gray" : "red"}
-                  //     >
-                  //       {chat?.latestMessage?.sender?.username ===
-                  //         chat.chatName && chat.isUser === false ? (
-                  //         <b>{`AnoUser-${chat.chatName.substr(-5)}`}</b>
-                  //       ) : (
-                  //         <b>{chat?.latestMessage?.sender?.username}</b>
-                  //       )}
-                  //       <b> : </b>
-                  //       {chat.latestMessage.content.length > 50
-                  //         ? chat.latestMessage.content.substring(0, 35) + "..."
-                  //         : chat.latestMessage.content}
-                  //     </Text>
-                  //   </Paper>
-                  // </Indicator>
-                  <Text key={chat._id}>{chat._id}</Text>
+                  <Indicator
+                    size={17}
+                    disabled={checkUnread(chat.latestMessage)}
+                    label="New"
+                    key={chat._id}
+                    position="top-left"
+                  >
+                    <Paper
+                      onClick={() => {
+                        setSelectedChat(chat);
+                        chatRead(chat);
+                        params(chat._id);
+                      }}
+                      bg={selectedChat === chat ? "#38B2AC" : "#E8E8E8"}
+                      color={selectedChat === chat ? "white" : "black"}
+                      onMouseEnter={(e) => (e.target.style.cursor = "pointer")}
+                    >
+                      <Text size="xl" ta={"center"}>
+                        {/* {chat.chatName.startsWith("user-") */}
+                        {/* ? `AnoUser-${chat.chatName.substr(-5)}` */}
+                        {/* : `${chat.chatName}`} */}
+                        {chat.chatName}
+                      </Text>
+                      <Text
+                        span
+                        size="md"
+                        c={checkUnread(chat.latestMessage) ? "gray" : "red"}
+                      >
+                        {chat?.latestMessage?.sender?.username ===
+                          chat.chatName && chat.isUser === false ? (
+                          <b>{`AnoUser-${chat.chatName.substr(-5)}`}</b>
+                        ) : (
+                          <b>{chat?.latestMessage?.sender?.username}</b>
+                        )}
+                        <b> : </b>
+                        {chat.latestMessage.content.length > 50
+                          ? chat.latestMessage.content.substring(0, 35) + "..."
+                          : chat.latestMessage.content}
+                      </Text>
+                    </Paper>
+                  </Indicator>
                 ))}
             </Stack>
           </GridCol>
