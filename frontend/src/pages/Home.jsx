32968778b596c49e73d72278055b61504b0ad6a3
Quan9 <boboylt000@gmail.com>
2024-07-13 16:17:00 +0700

    321

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -80,76 +80,77 @@ const Home = () => {
           if (e.key === "Enter") navigate();
         }}
       />
-      {/* <Categories /> */}
-      {data.length !== 0 ? (
-        <Grid overflow="hidden" p={10}>
-          {data.forEach((items) => {
-            return (
-              <GridCol
-                key={items._id}
-                order={
-                  items._id === "Phone" ? 1 : items._id === "Laptop" ? 2 : 3
-                }
-                mt={"sm"}
-              >
-                <Group align="flex-end" justify="flex-end">
-                  <Title order={2} ta="center" flex={1}>
-                    Latest {items._id}
-                  </Title>
-                  {items.data.length > 4 && (
-                    <UnstyledButton component={NavLink} to={`/${items._id}`}>
-                      more <IconPlayerTrackNext size={12} />
-                    </UnstyledButton>
-                  )}
-                </Group>
-                <Grid mt={"sm"}>
-                  {items.data.map((product, index) => (
-                    <GridCol
-                      span={{ lg: 3, md: 4, sm: 6, xs: 12 }}
-                      key={product._id + index}
-                      maw={{ xs: "50%" }}
-                    >
-                      <Card display={index === 4 && "none"} align={"center"}>
-                        <CardSection>
-                          <Image
-                            src={product.colors[0].image}
-                            alt="Image"
-                            h={300}
-                          />
-                        </CardSection>
-                        <CardSection>
-                          <Title order={4} fw={700}>
-                            {product.name}
-                          </Title>
-                          {product?.discount ? (
-                            <FormatPrice
-                              price={product.price}
-                              discount={product.discount}
+      <Flex>
+        {data.length !== 0 ? (
+          <Grid overflow="hidden" p={10}>
+            {data.forEach((items) => {
+              return (
+                <GridCol
+                  key={items._id}
+                  order={
+                    items._id === "Phone" ? 1 : items._id === "Laptop" ? 2 : 3
+                  }
+                  mt={"sm"}
+                >
+                  <Group align="flex-end" justify="flex-end">
+                    <Title order={2} ta="center" flex={1}>
+                      Latest {items._id}
+                    </Title>
+                    {items.data.length > 4 && (
+                      <UnstyledButton component={NavLink} to={`/${items._id}`}>
+                        more <IconPlayerTrackNext size={12} />
+                      </UnstyledButton>
+                    )}
+                  </Group>
+                  <Grid mt={"sm"}>
+                    {items.data.map((product, index) => (
+                      <GridCol
+                        span={{ lg: 3, md: 4, sm: 6, xs: 12 }}
+                        key={product._id + index}
+                        maw={{ xs: "50%" }}
+                      >
+                        <Card display={index === 4 && "none"} align={"center"}>
+                          <CardSection>
+                            <Image
+                              src={product.colors[0].image}
+                              alt="Image"
+                              h={300}
                             />
-                          ) : (
-                            <FormatPrice price={product.price} />
-                          )}
-                          <Button
-                            component={NavLink}
-                            to={`/${product.categories}/${product._id}`}
-                            variant="light"
-                          >
-                            Detail
-                          </Button>
-                        </CardSection>
-                      </Card>
-                    </GridCol>
-                  ))}
-                </Grid>
-              </GridCol>
-            );
-          })}
-        </Grid>
-      ) : (
-        <Center h={"100%"}>
-          <Loader type="dots" size={100} />
-        </Center>
-      )}
+                          </CardSection>
+                          <CardSection>
+                            <Title order={4} fw={700}>
+                              {product.name}
+                            </Title>
+                            {product?.discount ? (
+                              <FormatPrice
+                                price={product.price}
+                                discount={product.discount}
+                              />
+                            ) : (
+                              <FormatPrice price={product.price} />
+                            )}
+                            <Button
+                              component={NavLink}
+                              to={`/${product.categories}/${product._id}`}
+                              variant="light"
+                            >
+                              Detail
+                            </Button>
+                          </CardSection>
+                        </Card>
+                      </GridCol>
+                    ))}
+                  </Grid>
+                </GridCol>
+              );
+            })}
+          </Grid>
+        ) : (
+          <Center h={"100%"}>
+            <Loader type="dots" size={100} />
+          </Center>
+        )}
+      </Flex>
     </Container>
   );
 };
