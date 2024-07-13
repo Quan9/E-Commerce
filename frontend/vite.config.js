0197de56772b01166b0a058fd413c253a791ee2c
Quan9 <boboylt000@gmail.com>
2024-07-13 14:49:57 +0700

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,38 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: `${import.meta.env.VITE_URL_SERVER}`,
-        changeOrigin: true,
-        secure: true,
-        ws: true,
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  // import.meta.env.VITE_NAME available here with: process.env.VITE_NAME
+  // import.meta.env.VITE_PORT available here with: process.env.VITE_PORT
+
+  return defineConfig({
+    plugins: [react()],
+
+    server: {
+      proxy: {
+        "/api": {
+          target: process.env.VITE.URL.SERVER,
+          changeOrigin: true,
+          secure: false,
+          ws: true,
+        },
       },
     },
-  },
-});
+  });
+};
+// https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       "/api": {
+//         target: `${import.meta.env.VITE_URL_SERVER}`,
+//         changeOrigin: true,
+//         secure: false,
+//         ws: true,
+//       },
+//     },
+//   },
+// });
